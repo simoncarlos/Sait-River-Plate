c0259b0580591acb8f17f632deec88905679284f
Carlos Diego Simon Hurtado <diegoestushoi@gmail.com>
2022-10-28 20:04:34 -0300

    Agregado de header y footer

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () =>{
+    return(
+        <div className="h-screen bg-header relative flex items-center justify-between">
+            
+        </div>
+    );
+};
+
+export default Header;
